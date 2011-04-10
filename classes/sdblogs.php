e0cb7d0e371c0d570730583bd672dc99a8945be6
Chris Meller <chris@doesnthaveone.com>
2011-04-10 16:49:53 -0400

    Start off the base API class.

---
+++ classes/sdblogs.php
@@ -0,0 +1,26 @@
+<?php
+
+	class SDBLogs {
+		
+		protected $sdb = null;
+		
+		protected $domain;
+		
+		public function __construct ( ) {
+			
+			parent::__construct();
+			
+			require_once( Kohana::find_file( 'vendor', 'awstools/aws' ) );
+			
+			$this->data_domain = Kohana::config( 'sdb-logs-viewer.data_domain' );
+			$this->index_domain = Kohana::config( 'sdb-logs-viewer.index_domain' );
+			$aws_key = Kohana::config( 'sdb-logs-viewer.aws_key' );
+			$aws_secret = Kohana::config( 'sdb-logs-viewer.aws_secret' );
+			
+			$this->sdb = new SimpleDB( $aws_key, $aws_secret );
+			
+		}
+		
+	}
+
+?>
