f9c5a06a7728a0b1e706aa3caed09ce698a50efb
Chris Meller <chris@doesnthaveone.com>
2011-04-12 19:18:16 -0400

    Add the override Kohana class to replace the new numeric environment constants with useful textual values.

---
+++ application/classes/kohana.php
@@ -0,0 +1,12 @@
+<?php
+
+	class Kohana extends Kohana_Core {
+		
+		const DEVELOPMENT = 'development';
+		const PRODUCTION = 'production';
+		const STAGING = 'staging';
+		const TESTING = 'testing';
+		
+	}
+
+?>
