f7689e9db3c1a47e0dfb551a02b7657ff933ee1b
Chris Meller <chris@doesnthaveone.com>
2011-04-12 23:53:38 -0400

    Update the name of the config file that holes our information.

--- application/classes/sdblogs.php
+++ application/classes/sdblogs.php
@@ -11,10 +11,10 @@
 			
 			require_once( Kohana::find_file( 'vendor', 'awstools/aws' ) );
 			
-			$this->data_domain = Kohana::config( 'sdb-logs-viewer.data_domain' );
-			$this->index_domain = Kohana::config( 'sdb-logs-viewer.index_domain' );
-			$aws_key = Kohana::config( 'sdb-logs-viewer.aws_key' );
-			$aws_secret = Kohana::config( 'sdb-logs-viewer.aws_secret' );
+			$this->data_domain = Kohana::config( 'logs.data_domain' );
+			$this->index_domain = Kohana::config( 'logs.index_domain' );
+			$aws_key = Kohana::config( 'logs.aws_key' );
+			$aws_secret = Kohana::config( 'logs.aws_secret' );
 			
 			$this->sdb = new SimpleDB( $aws_key, $aws_secret );
 			
