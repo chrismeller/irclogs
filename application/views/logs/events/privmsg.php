af38dfdb0350e2c456fb35cadeb81d15077e6235
Chris Meller <chris@doesnthaveone.com>
2011-04-13 15:19:02 -0400

    Automagically convert links to... links.

--- application/views/logs/events/privmsg.php
+++ application/views/logs/events/privmsg.php
@@ -2,6 +2,6 @@
 
 	$nick = '<' . $line['nick'] . '>';
 
-	echo '<span class="privmsg"><span class="nick">' . HTML::chars( $nick ) . '</span> ' . HTML::chars( $line['message'] ) . '</span>';
+	echo '<span class="privmsg"><span class="nick">' . HTML::chars( $nick ) . '</span> ' . Text::auto_link( HTML::chars( $line['message'] ) ) . '</span>';
 
 ?>
