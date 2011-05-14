7c854ba668db1fa02c2aeb3b53dc903d00b210cc
Chris Meller <chris@doesnthaveone.com>
2011-05-14 13:17:21 -0400

    Tweak all the base Logs javascript so we include next token (including a workaround for line breaks) and trim the channel name.

--- application/views/logs/js.php
+++ application/views/logs/js.php
@@ -1,18 +1,26 @@
 <?php
 
-	$keys = array( 'channel', 'year', 'month', 'day' );
+	$keys = array( 'channel', 'year', 'month', 'day', 'next_token' );
+	
+	// urlencode the next token so it doesn't break the javascript
+	$next_token = urlencode( $next_token );
+	
+	// strip the leading character from the channel
+	$channel = SDBLogs::trim_chan( $channel );
 	
 	$js = array();
 	foreach ( $keys as $key ) {
 		if ( isset( $$key ) ) {
-			$js[] = "Logs.$key: '{$$key}'";
+			$js[] = "Logs.$key = '{$$key}';";
 		}
 		else {
-			$js[] = "Logs.$key: ''";
+			$js[] = "Logs.$key = '';";
 		}
 	}
 	
-	$js = implode( ";\n", $js ) . ';';
+	$js[] = 'Logs.next_token = unescape( Logs.next_token );';
+	
+	$js = implode( "\n", $js );
 	
 	echo '<script type="text/javascript">' . $js . '</script>';
 
